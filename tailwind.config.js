07cb0f15a5f19babdabcdcceedaa988ccbbbd551
taroodr <n.kotaro.0101@gmail.com>
2021-02-25 00:59:01 +0900

    fix color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            a: {
+              color: "#34D399",
+              "&:hover": {
+                color: "#059699",
+              },
+            },
+          },
+        },
+      },
+    },
   },
   variants: {
     extend: {},
